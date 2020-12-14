8367f016d970c2462e53159eeb65e28f00edc2bd
Rainard <rainard.human@gmail.com>
2020-12-14 18:23:12 +0200

    added routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,15 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './website/home/home.component';
 import { AuthComponent } from './website/auth/auth.component';
+import { PageNotFoundComponent } from './website/page-not-found/page-not-found.component';
+import { ProfileComponent } from './website/profile/profile.component';
 
 const routes: Routes = [
-  { path: 'home', component: HomeComponent },
+  { path: '', component: HomeComponent },
   { path: 'auth', component: AuthComponent },
+  { path: 'page-not-found', component: PageNotFoundComponent },
+  { path: 'profile', component: ProfileComponent },
+  { path: '**', redirectTo: '' },
 ];
 
 @NgModule({
