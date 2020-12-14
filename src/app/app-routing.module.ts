f4e1c01a3b77b4b2492c4f0e48ead1111a2eb39a
Rainard <rainard.human@gmail.com>
2020-12-14 23:32:17 +0200

    sleep time

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,10 +4,14 @@ import { HomeComponent } from './website/home/home.component';
 import { AuthComponent } from './website/auth/auth.component';
 import { PageNotFoundComponent } from './website/page-not-found/page-not-found.component';
 import { ProfileComponent } from './website/profile/profile.component';
+import { DealComponent } from './website/deal/deal.component';
+import { LeadComponent } from './website/lead/lead.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'auth', component: AuthComponent },
+  { path: 'deals', component: DealComponent },
+  { path: 'leads', component: LeadComponent },
   { path: 'page-not-found', component: PageNotFoundComponent },
   { path: 'profile', component: ProfileComponent },
   { path: '**', redirectTo: '' },
